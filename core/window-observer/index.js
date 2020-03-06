92ad0ff5918c0c0d2c8c54070bb3e2328f859b1d
Kyza <40003440+KyzaGitHub@users.noreply.github.com>
2020-03-06 14:44:41 -0500

    Optimized WindowObserver

--- core/window-observer/index.js
+++ core/window-observer/index.js
@@ -1,5 +1,6 @@
 (() => {
 	const { windowManager } = require("../node-window-manager");
+	windowManager.requestAccessibility();
 	const { desktopCapturer } = require("electron");
 
 	const makeSafe = (text) => {
@@ -27,81 +28,99 @@
 	];
 	let allWindows = [];
 
-	module.exports = {
-		observe: (callback) => {
-			windowManager.requestAccessibility();
-			setInterval(() => {
-				let windowsAdded = [];
-				let windowsRemoved = [];
+	let callbacks = [];
 
-				desktopCapturer
-					.getSources({
-						types: ["window"],
-						thumbnailSize: { width: 512, height: 512 },
-						fetchWindowIcons: true
-					})
-					.then(async (sources) => {
-						// Loop over all of the saved windows.
-						// Remove all windows that don't exist anymore.
-						for (let i = 0; i < allWindows.length; i++) {
-							const savedWindow = allWindows[i];
-							// If the saved window isn't in the list of windows...
+	setInterval(() => {
+		if (callbacks.length > 0) {
+			let windowsAdded = [];
+			let windowsRemoved = [];
+
+			desktopCapturer
+				.getSources({
+					types: ["window"],
+					thumbnailSize: { width: 512, height: 512 },
+					fetchWindowIcons: true
+				})
+				.then(async (sources) => {
+					// Loop over all of the saved windows.
+					// Remove all windows that don't exist anymore.
+					for (let i = 0; i < allWindows.length; i++) {
+						const savedWindow = allWindows[i];
+						// If the saved window isn't in the list of windows...
+						if (
+							!sources.find((source) => {
+								if (source.id == savedWindow.desktopCapturer.id) {
+									return true;
+								}
+							})
+						) {
+							allWindows.splice(i, 1);
+							windowsRemoved.push(savedWindow);
+						}
+					}
+
+					// Loop over all of the desktop window sources.
+					// Add all new windows.
+					for (let i = 0; i < sources.length; i++) {
+						const source = sources[i];
+
+						// Make sure the window isn't one that needs to be avoided.
+						if (windowsToAvoid.indexOf(source.name) < 0) {
+							// Make sure the window isn't already in the list of windows.
 							if (
-								!sources.find((source) => {
-									if (source.id == savedWindow.desktopCapturer.id) {
+								!allWindows.find((window) => {
+									if (window.desktopCapturer.id == source.id) {
 										return true;
 									}
 								})
 							) {
-                allWindows.splice(i, 1);
-								windowsRemoved.push(savedWindow);
-							}
-						}
+								const windowManagerWindows = windowManager.getWindows();
 
-						// Loop over all of the desktop window sources.
-						// Add all new windows.
-						for (let i = 0; i < sources.length; i++) {
-							const source = sources[i];
-
-							// Make sure the window isn't one that needs to be avoided.
-							if (windowsToAvoid.indexOf(source.name) < 0) {
-								// Make sure the window isn't already in the list of windows.
-								if (
-									!allWindows.find((window) => {
-										if (window.desktopCapturer.id == source.id) {
-											return true;
-										}
-									})
-								) {
-									const windowManagerWindows = windowManager.getWindows();
-
-									// Loop over all of the windows that node-window-manager finds.
-									for (let j = 0; j < windowManagerWindows.length; j++) {
-										const windowManagerWindow = windowManagerWindows[j];
-										// Match the desktop window source with the node-window-manager window.
-										if (
-											parseInt(source.id.replace("window:", "").replace(":0", "")) ==
-											windowManagerWindow.id
-										) {
-											allWindows.push({
-												desktopCapturer: source,
-												windowManager: windowManagerWindow
-											});
-											windowsAdded.push({
-												desktopCapturer: source,
-												windowManager: windowManagerWindow
-											});
-										}
+								// Loop over all of the windows that node-window-manager finds.
+								for (let j = 0; j < windowManagerWindows.length; j++) {
+									const windowManagerWindow = windowManagerWindows[j];
+									// Match the desktop window source with the node-window-manager window.
+									if (
+										parseInt(source.id.replace("window:", "").replace(":0", "")) ==
+										windowManagerWindow.id
+									) {
+										allWindows.push({
+											desktopCapturer: source,
+											windowManager: windowManagerWindow
+										});
+										windowsAdded.push({
+											desktopCapturer: source,
+											windowManager: windowManagerWindow
+										});
 									}
 								}
 							}
 						}
+					}
 
-						if (windowsAdded.length > 0 || windowsRemoved.length > 0) {
-							callback(allWindows, windowsAdded, windowsRemoved);
+					if (windowsAdded.length > 0 || windowsRemoved.length > 0) {
+						for (let i = 0; i < callbacks.length; i++) {
+							try {
+								callbacks[i](allWindows, windowsAdded, windowsRemoved);
+							} catch (e) {
+								console.error(e);
+							}
 						}
-					});
-			}, 1000);
+					}
+				});
+		}
+	}, 1000);
+
+	function remove(array, element) {
+		return array.filter((el) => el !== element);
+	}
+
+	module.exports = {
+		observe: (callback) => {
+			callbacks.push(callback);
+		},
+		unobserve: (callback) => {
+			remove(callbacks, callback);
 		}
 	};
 })();
